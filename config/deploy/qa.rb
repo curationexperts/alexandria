71273de042d60796dcfb512b591e3d6009eea495
Bess Sadler <bess@curationexperts.com>
2018-02-07 09:50:42 -0500

    Use curationexperts repo

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -2,6 +2,7 @@
 
 set :stage, :qa
 set :rails_env, "production"
+set :repo_url, "https://github.com/curationexperts/alexandria.git"
 server "qa-alexandria.curationexperts.com",
        user: "deploy",
        roles: [:web, :app, :db, :resque_pool]
