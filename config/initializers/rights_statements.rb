fae043e3126c546b22ce5b34844cb1db7797dfa5
Bess Sadler <bess@curationexperts.com>
2018-02-14 08:48:24 -0500

    Update copyright unknown statement
    Europeana has updated their terms and ask that users update
    references accordingly. See https://www.europeana.eu/portal/en/rights/unknown.html
    "copyright unknown" should now be "Copyright not evaluated"

--- config/initializers/rights_statements.rb
+++ config/initializers/rights_statements.rb
@@ -4,10 +4,10 @@
 # this puts a valid label into Marmotta so we don't hit
 # Europeana multiple times without effect.
 stmt = ControlledVocabularies::RightsStatement.new(
-  "http://www.europeana.eu/rights/unknown/"
+  "http://rightsstatements.org/vocab/CNE/1.0/"
 )
 
 stmt << RDF::Statement.new(stmt.rdf_subject,
-                           RDF::RDFS.label, "Unknown copyright status")
+                           RDF::RDFS.label, "Copyright not evaluated")
 
 stmt.persist!
