d2265171456e105a87c565f7c949d393e5c1aaa8
Bess Sadler <bess@curationexperts.com>
2018-02-14 08:23:26 -0500

    Add sandbox deploy target

---
+++ config/deploy/sandbox.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+set :stage, :sandbox
+set :rails_env, "production"
+
+server "qa-alexandria.curationexperts.com",
+       user: "deploy",
+       roles: [:web, :app, :db, :resque_pool]
+
+set :linked_files, %w[
+  config/database.yml
+  config/ezid.yml
+  config/fedora.yml
+  config/secrets.yml
+  config/solr.yml
+  config/blacklight.yml
+]
+
+# Extended Server Syntax
+# ======================
+# This can be used to drop a more detailed server definition into the
+# server list. The second argument is a, or duck-types, Hash and is
+# used to set extended properties on the server.
+
+# Custom SSH Options
+# ==================
+# You may pass any option but keep in mind that net/ssh understands a
+# limited set of options, consult[net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start).
+#
+# Global options
+# --------------
+#  set :ssh_options, {
+#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+#    forward_agent: false,
+#    auth_methods: %w(password)
+#  }
+#
+# And/or per server (overrides global)
+# ------------------------------------
+# server 'example.com',
+#   user: 'user_name',
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: 'user_name', # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: 'please use keys'
+#   }
