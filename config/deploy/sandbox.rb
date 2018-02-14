4cdfb64589780a30cde3b48d16ca39bb23be014d
Bess Sadler <bess@curationexperts.com>
2018-02-14 16:20:11 -0500

    update server name

--- config/deploy/sandbox.rb
+++ config/deploy/sandbox.rb
@@ -4,7 +4,7 @@ set :stage, :sandbox
 set :rails_env, "production"
 set :repo_url, "https://github.com/curationexperts/alexandria.git"
 
-server "qa-alexandria.curationexperts.com",
+server "adrl-sandbox.curationexperts.com",
        user: "deploy",
        roles: [:web, :app, :db, :resque_pool]
 
